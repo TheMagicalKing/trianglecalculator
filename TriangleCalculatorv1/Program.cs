d28dcc851dce276ff4153ae4074b21c8b0a0080f
Nicki <alternativelenicki@live.dk>
2021-05-26 19:17:18 +0200

    small adjustments to the repository, also versioned the right files

---
+++ TriangleCalculatorv1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TriangleCalculatorv1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation triCalc = new Calculation();
+
+            Calculation.CalculateInt();
+        }
+    }
+}
